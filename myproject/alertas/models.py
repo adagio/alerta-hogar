d36bb551503493a0e0383c7006d5d84a18b74d3a
Franco Cedillo <franco.cedillo@gmail.com>
2020-03-22 00:42:08 -0500

    aggression text to TextField

--- myproject/alertas/models.py
+++ myproject/alertas/models.py
@@ -8,8 +8,9 @@ class Place(models.Model):
         return self.name
 
 class Aggression(models.Model):
-    aggression_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    aggression_text = models.TextField()
+    time = models.DateTimeField('date occurred')
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published', auto_now=True)
     def __str__(self):
         return self.aggression_text
