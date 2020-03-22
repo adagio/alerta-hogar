dea199b6ec00c3207fc5eca878e031af6fc831f5
Franco Cedillo <franco.cedillo@gmail.com>
2020-03-22 16:14:36 -0500

    add template for alertas index

---
+++ myproject/alertas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .import views
+
+app_name = 'alertas'
+urlpatterns = [
+    path('', views.index, name='index')
+]
--- myproject/alertas/views.py
+++ myproject/alertas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+from .models import Aggression
+
+def index(request):
+    latest_aggression_list = Aggression.objects.all()
+    context = {
+        'latest_aggression_list': latest_aggression_list,
+    }
+    return render(request, 'alertas/index.html', context)
