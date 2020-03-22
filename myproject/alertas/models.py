fbe5631a4682464c6d271f1bcbf7534b4708d834
Franco Cedillo <franco.cedillo@gmail.com>
2020-03-22 00:00:22 -0500

    add representation function

--- myproject/alertas/models.py
+++ myproject/alertas/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 class Place(models.Model):
     name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
 
 class Aggression(models.Model):
     aggression_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.aggression_text
