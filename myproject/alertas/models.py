78eddbc2773c4f7f6ca845e5b4f8c1274788f6c1
Franco Cedillo <franco.cedillo@gmail.com>
2020-03-21 23:52:09 -0500

    add Aggression model

--- myproject/alertas/models.py
+++ myproject/alertas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=30)
+
+class Aggression(models.Model):
+    aggression_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
